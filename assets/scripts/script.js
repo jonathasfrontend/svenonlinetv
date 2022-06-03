2f08e60ae57185cc07ebae6b995a271206a11e8d
Jonathas <jonathass5678@gmail.com>
2022-06-03 16:24:51 -0300

    sven

---
+++ assets/scripts/script.js
@@ -0,0 +1,51 @@
+const filmesseriescontent = document.querySelector('.filmes-series');
+const desenhosanimados = document.querySelector('.desenhos-animados');
+const variedades = document.querySelector('.variedades');
+
+fetch('https://apitvonline.herokuapp.com/clients').then(response=> response.json()).then(data=>{
+
+    data.forEach(element => {
+        element.filmesseries.map(filme => {
+
+            filmesseriescontent.innerHTML += `
+                <div class="card">
+                <img src="${filme.image}" alt="Sven tv online">
+                <div class="titles-card">
+                    <a href="${filme.link}">Assistir</a>
+                    <h1>${filme.title}</h1>
+                </div>
+                </div>
+                `;
+        })
+    })
+
+    data.forEach(element => {
+        element.desenhosanimados.map(filme => {
+
+            desenhosanimados.innerHTML += `
+                <div class="card">
+                <img src="${filme.image}" alt="Sven tv online">
+                <div class="titles-card">
+                    <a href="${filme.link}">Assistir</a>
+                    <h1>${filme.title}</h1>
+                </div>
+                </div>
+                `;
+        })
+    })
+    data.forEach(element => {
+        element.variedades.map(filme => {
+
+            variedades.innerHTML += `
+                <div class="card">
+                <img src="${filme.image}" alt="Sven tv online">
+                <div class="titles-card">
+                    <a href="${filme.link}">Assistir</a>
+                    <h1>${filme.title}</h1>
+                </div>
+                </div>
+                `;
+        })
+    })
+    
+})
