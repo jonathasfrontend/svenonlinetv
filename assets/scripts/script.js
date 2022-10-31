6981829fc7acdd720c8fb047650ec9d9a88fb19c
Jonathas <jonathass5678@gmail.com>
2022-10-31 10:47:32 -0300

    sven

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,24 @@
 const filmesseriescontent = document.querySelector('.filmes-series');
 const desenhosanimados = document.querySelector('.desenhos-animados');
 const variedades = document.querySelector('.variedades');
+const banner = document.getElementById('banner');
 
-fetch('https://apitvonline.herokuapp.com/clients').then(response=> response.json()).then(data=>{
+fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&language=en-US&page=2').then(response=> response.json()).then(data=>{
+    data.results.map((i,j)=>{
+        if(j > 18)
+        return;
+        if(j == 0){
+            banner.innerHTML+=`
+            <img src="https://image.tmdb.org/t/p/original`+i.backdrop_path+`" alt="">
+            `;
+        }
+    })
+})
+
+fetch('https://apitvonline.herokuapp.com/clients').then(response => response.json()).then(data=>{
 
     data.forEach(element => {
-        element.filmesseries.map(filme => {
+        element.filmesseries.map(filme =>  {
 
             filmesseriescontent.innerHTML += `
                 <div class="card">
