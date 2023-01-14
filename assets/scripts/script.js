71115603039f8f860bb8ebad52bbc45e47f6a340
Jonathas <jonathass5678@gmail.com>
2023-01-14 14:52:57 -0300

    sventv

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,10 @@
 const filmesseriescontent = document.querySelector('.filmes-series');
 const desenhosanimados = document.querySelector('.desenhos-animados');
 const variedades = document.querySelector('.variedades');
+const esports = document.querySelector('.esports');
 const banner = document.getElementById('banner');
 
-fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&language=en-US&page=2')
+fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&language=en-US&page=1')
 .then(response=> response.json())
 .then(data=>{
     data.results.map((i,j)=>{      
@@ -64,6 +65,20 @@ data.forEach(element => {
     element.variedades.map(filme => {
 
         variedades.innerHTML += `
+            <div class="card">
+            <img src="${filme.image}" alt="Sven tv online" class="img-banner-play">
+            <div class="titles-card">
+                    <a href="${filme.link}"><img src="./assets/img/tv.svg" alt=""><h1>${filme.title}</h1></a>
+            </div>
+            </div>
+            `;
+    })
+})
+    
+data.forEach(element => {
+    element.sport.map(filme => {
+
+        esports.innerHTML += `
             <div class="card">
             <img src="${filme.image}" alt="Sven tv online" class="img-banner-play">
             <div class="titles-card">
